5926787a1e5e1fc5a5d40ba364d11bbd958d533b
aseaboyer <aseaboyer@gmail.com>
2014-10-27 00:01:36 -0700

    fix bad ref

--- player.js
+++ player.js
@@ -1,20 +1,21 @@
 function Player( ) {
 	return {
 		update: function ( c, f, hand1, hand2 ) {
-			if (hand1 && hand2) {
-				this.hands.right.x = hand1.palmPosition[0];
+			var und;
+			if( hand1 != und && hand2 != und ) {
+			/*	this.hands.right.x = hand1.palmPosition[0];
 				this.hands.right.y = hand1.palmPosition[1];
 				this.hands.right.z = hand1.palmPosition[2];
 				this.hands.left.x = hand2.palmPosition[0];
 				this.hands.left.y = hand2.palmPosition[1];
-				this.hands.left.z = hand2.palmPosition[2];
-			}
+				this.hands.left.z = hand2.palmPosition[2];*/
 			
 		//	console.log( hand1.palmPosition[0] + " + " + hand2.palmPosition[0] + " = " + (hand1.palmPosition[0] + hand2.palmPosition[0]) );
-			var orbTargetX = (hand1.palmPosition[0] + hand2.palmPosition[0]) * 0.5;
-			var orbTargetZ = (hand1.palmPosition[2] + hand2.palmPosition[2]) * 0.5;
-			this.orb.target.x = orbTargetX;
-			this.orb.target.z = orbTargetZ;
+				var orbTargetX = (hand1.palmPosition[0] + hand2.palmPosition[0]) * 0.5;
+				var orbTargetZ = (hand1.palmPosition[2] + hand2.palmPosition[2]) * 0.5;
+				this.orb.target.x = orbTargetX;
+				this.orb.target.z = orbTargetZ;
+			}
 			
 			//this.hands.drawHands( cont, w, h );
 			this.orb.draw( cont, w, h );
