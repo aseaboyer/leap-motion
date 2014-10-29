76633279eefdfd0aee50580f24e5bfe6aec27448
aseaboyer <aseaboyer@gmail.com>
2014-10-28 18:26:46 -0700

    block and player changes

--- js/block.js
+++ js/block.js
@@ -1,17 +1,21 @@
-function GameInfo( uic, stbtn, rstbtn, rsmbtn ) {
+function Block( uic, stbtn, rstbtn, rsmbtn ) {
 	return {
-		speed: 10
+		speed: 100,
 		dims: {
 			x: 100,
 			y: 100
-		}
+		},
+		dir: { 
+			x: 0,
+			y: 0
+		},
 		start: {
 			x: 0,
 			y: 0
-		}
+		},
 		current: {
 			x: 0,
 			y: 0
-		}
+		},
 	}
 }
