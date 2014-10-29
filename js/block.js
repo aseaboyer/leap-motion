d8cddc80c7efde596593a8f968f6caeb4250dbdf
aseaboyer <aseaboyer@gmail.com>
2014-10-28 17:17:08 -0700

    start adding block objects

---
+++ js/block.js
@@ -0,0 +1,17 @@
+function GameInfo( uic, stbtn, rstbtn, rsmbtn ) {
+	return {
+		speed: 10
+		dims: {
+			x: 100,
+			y: 100
+		}
+		start: {
+			x: 0,
+			y: 0
+		}
+		current: {
+			x: 0,
+			y: 0
+		}
+	}
+}
