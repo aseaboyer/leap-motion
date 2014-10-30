f5919f18767cc4b3005e7facd751cea21db7d48a
aseaboyer <aseaboyer@gmail.com>
2014-10-29 23:33:12 -0700

    writing reminders for later

--- js/block.js
+++ js/block.js
@@ -13,13 +13,13 @@ function Block( uic, stbtn, rstbtn, rsmbtn ) {
 			this.dims.x = dimsX;
 			this.dims.y = dimsY;
 			this.speed = speed;
-			this.position = 100;
+			this.position = 100; // this will need to be -dimsY
 		},
 		update: function ( c, timeMod ) {
 			// update the position
 			this.draw( c );
 		},
-		draw: function ( c ) {
+		draw: function ( c ) { // this will be more complex, drawing arrows up for sub zero and such
 			c.rect( this.position.x,this.position.y, this.dims.x,this.dims.y );
 			c.fillStyle = "#c8c8a9";
 			c.fill();
