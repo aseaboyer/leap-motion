ad7dfcd45f60a0b9e6b003d6fe2236db0ac7f4eb
aseaboyer <aseaboyer@gmail.com>
2014-10-30 00:13:34 -0700

    flipping block colors

--- js/block.js
+++ js/block.js
@@ -21,11 +21,11 @@ function Block( uic, stbtn, rstbtn, rsmbtn ) {
 			this.draw( c );
 		},
 		draw: function ( c ) { // this will be more complex, drawing arrows up for sub zero and such
-			c.fillStyle = "#83af9b";
+			c.fillStyle = "#c8c8a9";
 			c.rect( this.position.x,this.position.y, this.dims.x,this.dims.y );
 			c.fill();
 			
-			c.fillStyle = "#c8c8a9";
+			c.fillStyle = "#83af9b";
 			c.rect( this.position.x,this.position.y, this.dims.x,(this.dims.y - 20) );
 			c.fill();
 		},
