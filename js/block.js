b5a89be2db0c8f4eeab0ceb6c31eb81e5c8305f9
aseaboyer <aseaboyer@gmail.com>
2014-10-29 22:46:24 -0700

    save blocks and draw them

--- js/block.js
+++ js/block.js
@@ -5,9 +5,24 @@ function Block( uic, stbtn, rstbtn, rsmbtn ) {
 			x: 100,
 			y: 0
 		},
-		current: {
+		position: {
 			x: 0,
 			y: 0
+		},
+		set: function ( dimsX, dimsY, yPos, speed ) {
+			this.dims.x = dimsX;
+			this.dims.y = dimsY;
+			this.speed = speed;
+			this.position = 100;
+		},
+		update: function ( c, timeMod ) {
+			// update the position
+			this.draw( c );
+		},
+		draw: function ( c ) {
+			c.rect(20,20,150,100);
+			c.fillStyle="red";
+			c.fill();
 		},
 	}
 }
