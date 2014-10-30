ae2743141e5199aeec03bbf08f802a0d907e833e
aseaboyer <aseaboyer@gmail.com>
2014-10-30 00:14:47 -0700

    ....forgot to start a new path

--- js/block.js
+++ js/block.js
@@ -21,10 +21,12 @@ function Block( uic, stbtn, rstbtn, rsmbtn ) {
 			this.draw( c );
 		},
 		draw: function ( c ) { // this will be more complex, drawing arrows up for sub zero and such
+			c.beginPath();
 			c.fillStyle = "#c8c8a9";
 			c.rect( this.position.x,this.position.y, this.dims.x,this.dims.y );
 			c.fill();
 			
+			c.beginPath();
 			c.fillStyle = "#83af9b";
 			c.rect( this.position.x,this.position.y, this.dims.x,(this.dims.y - 20) );
 			c.fill();
