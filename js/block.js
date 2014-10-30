b727025f09bea2eec03645a2b38fa1d0b8942237
aseaboyer <aseaboyer@gmail.com>
2014-10-29 23:15:02 -0700

    set block draw

--- js/block.js
+++ js/block.js
@@ -20,8 +20,12 @@ function Block( uic, stbtn, rstbtn, rsmbtn ) {
 			this.draw( c );
 		},
 		draw: function ( c ) {
-			c.rect(20,20,150,100);
-			c.fillStyle="red";
+			c.rect( this.position.x,this.position.x, this.dims.x,this.dims.y );
+			c.fillStyle = "#c8c8a9";
+			c.fill();
+			
+			c.rect( this.position.x,this.position.x, this.dims.x,(this.dims.y - 20) );
+			c.fillStyle = "#83af9b";
 			c.fill();
 		},
 	}
